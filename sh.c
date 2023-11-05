3c2e3a6cbbf6a9969800dd0fae9eee9ec26edd08
Patrick Connor <patrickvip35@gmail.com>
2023-11-05 18:46:06 -0500

    weird error with tcgetpid

--- sh.c
+++ sh.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include "jobs.h"
 
 size_t count = 1024;
 
@@ -20,7 +21,8 @@ size_t count = 1024;
      */
 
 void error_reset_handler(char *msg, char *tokens[count / 2],
-                         char *argv[count / 2]) {
+                         char *argv[count / 2])
+{
     fprintf(stderr, "%s\n", msg);
     memset(tokens, '\0', count / 2 * sizeof(char *));
     memset(argv, '\0', count / 2 * sizeof(char *));
@@ -39,7 +41,8 @@ input_redirect_path - used to store the file path of "<" redirection
 */
 void parse(char buffer[count], char *tokens[count / 2], char *argv[count / 2],
            int *argc, char **output_append_path, char **output_redirect_path,
-           char **input_redirect_path) {
+           char **input_redirect_path)
+{
     // Counts instances of output redirection
     int output_redirect_count = 0;
     // Used to track what type of redirection the previous token was
@@ -50,57 +53,67 @@ void parse(char buffer[count], char *tokens[count / 2], char *argv[count / 2],
     // Get the first token
     char *token_pointer = strtok(buffer, " \n\t");
     // Loop while input exists
-    while (token_pointer != NULL) {
+    while (token_pointer != NULL)
+    {
         // Flags wether the current token is the corresponding redirection
         // symbol
         int output_redirect_flag = !strcmp(token_pointer, ">");
         int input_redirect_flag = !strcmp(token_pointer, "<");
         int output_append_flag = !strcmp(token_pointer, ">>");
 
-        if (saw_rdr_flag != 0) {
+        if (saw_rdr_flag != 0)
+        {
             // If the previous instance and current instance are both
             // redirection symbols through an error
-            if (output_append_flag || output_redirect_flag) {
+            if (output_append_flag || output_redirect_flag)
+            {
                 error_reset_handler(
                     "syntax error: output file is a redirection symbol", tokens,
                     argv);
                 return;
-            } else if (input_redirect_flag) {
+            }
+            else if (input_redirect_flag)
+            {
                 error_reset_handler(
                     "syntax error: input file is a redirection symbol", tokens,
                     argv);
                 return;
-            } else {
+            }
+            else
+            {
                 // Set path files appropriately if previous instance was a
                 // redirection symbol
-                switch (saw_rdr_flag) {
-                    case 1:
-                        (*output_redirect_path) = token_pointer;
-                        saw_rdr_flag = 0;
-                        break;
-                    case 2:
-                        (*input_redirect_path) = token_pointer;
-                        saw_rdr_flag = 0;
-                        break;
-                    case 3:
-                        (*output_append_path) = token_pointer;
-                        saw_rdr_flag = 0;
-                        break;
-                    default:
-                        break;
+                switch (saw_rdr_flag)
+                {
+                case 1:
+                    (*output_redirect_path) = token_pointer;
+                    saw_rdr_flag = 0;
+                    break;
+                case 2:
+                    (*input_redirect_path) = token_pointer;
+                    saw_rdr_flag = 0;
+                    break;
+                case 3:
+                    (*output_append_path) = token_pointer;
+                    saw_rdr_flag = 0;
+                    break;
+                default:
+                    break;
                 }
                 token_pointer = strtok('\0', " \n\t");
                 // Check if the next token is null, (i.e no file path after
                 // ouput symbol)
                 if ((saw_rdr_flag == 1 || saw_rdr_flag == 3) &&
-                    token_pointer == NULL) {
+                    token_pointer == NULL)
+                {
                     error_reset_handler("syntax error: no output file", tokens,
                                         argv);
                     return;
                 }
                 // Check if the next token is null, (i.e no file path after
                 // input symbol)
-                else if (saw_rdr_flag == 2 && token_pointer == NULL) {
+                else if (saw_rdr_flag == 2 && token_pointer == NULL)
+                {
                     error_reset_handler("syntax error: no input file", tokens,
                                         argv);
                     return;
@@ -111,56 +124,73 @@ void parse(char buffer[count], char *tokens[count / 2], char *argv[count / 2],
         }
         // Count redirect symbols in input line, if more than one exists print
         // error
-        if (input_redirect_flag) {
-            if (input_redirect_count != 0) {
+        if (input_redirect_flag)
+        {
+            if (input_redirect_count != 0)
+            {
                 error_reset_handler("syntax error: multiple input files",
                                     tokens, argv);
                 return;
-            } else {
+            }
+            else
+            {
                 input_redirect_count += 1;
             }
-        } else if (output_redirect_flag || output_append_flag) {
-            if (output_redirect_count != 0) {
+        }
+        else if (output_redirect_flag || output_append_flag)
+        {
+            if (output_redirect_count != 0)
+            {
                 error_reset_handler("syntax error: mulitple output files",
                                     tokens, argv);
                 return;
-            } else {
+            }
+            else
+            {
                 output_redirect_count += 1;
             }
         }
 
-        if (output_redirect_flag) {
+        if (output_redirect_flag)
+        {
             // Set corresponding flag for next instance
             saw_rdr_flag = 1;
             token_pointer = strtok('\0', " \n\t");
             // Check if the next token is null, (i.e no file path after
             // redirection symbol)
-            if (token_pointer == NULL) {
+            if (token_pointer == NULL)
+            {
                 error_reset_handler("syntax error: no output file", tokens,
                                     argv);
                 return;
             }
             // Next instantiation of the loop
             continue;
-        } else if (input_redirect_flag) {
+        }
+        else if (input_redirect_flag)
+        {
             // Set corresponding flag for next instance
             saw_rdr_flag = 2;
             token_pointer = strtok('\0', " \n\t");
             // Check if the next token is null, (i.e no file path after
             // redirection symbol)
-            if (token_pointer == NULL) {
+            if (token_pointer == NULL)
+            {
                 error_reset_handler("syntax error: no input file", tokens,
                                     argv);
                 return;
             }
             continue;
-        } else if (output_append_flag) {
+        }
+        else if (output_append_flag)
+        {
             // Set corresponding flag for next instance
             saw_rdr_flag = 3;
             token_pointer = strtok('\0', " \n\t");
             // Check if the next token is null, (i.e no file path after
             // redirection symbol)
-            if (token_pointer == NULL) {
+            if (token_pointer == NULL)
+            {
                 error_reset_handler("syntax error: no output file", tokens,
                                     argv);
                 return;
@@ -170,18 +200,24 @@ void parse(char buffer[count], char *tokens[count / 2], char *argv[count / 2],
         tokens[index] = token_pointer;
         // Check if index is 0, check if first token is a path and move
         // appropriately
-        if (index == 0) {
+        if (index == 0)
+        {
             // In order to set the first ARGV, where the last slash is
             char *last_slash = strrchr(tokens[0], '/');
-            if (last_slash != NULL) {
+            if (last_slash != NULL)
+            {
                 argv[index] = last_slash + 1;
                 // Count argument counter each time a token is added to argv
                 (*argc)++;
-            } else {
+            }
+            else
+            {
                 argv[index] = token_pointer;
                 (*argc)++;
             }
-        } else {
+        }
+        else
+        {
             argv[index] = token_pointer;
             (*argc)++;
         }
@@ -193,7 +229,8 @@ void parse(char buffer[count], char *tokens[count / 2], char *argv[count / 2],
     // command erors.
     if ((*output_redirect_path || *input_redirect_path ||
          *output_append_path) &&
-        (*argv) == NULL) {
+        (*argv) == NULL)
+    {
         error_reset_handler("error:redirects with no command", tokens, argv);
         return;
     }
@@ -206,40 +243,52 @@ pointer to the output_redirect_path saved from parse output_append - pointer to
 the output_append_path saved from parse
 */
 void io_redirection(char **input_redirect_path, char **output_redirect_path,
-                    char **output_append) {
-    if ((*input_redirect_path)) {
-        if (close(0) == -1) {
+                    char **output_append)
+{
+    if ((*input_redirect_path))
+    {
+        if (close(0) == -1)
+        {
             perror("close");
             exit(1);
         }
-        if (open((*input_redirect_path), O_RDONLY, 0666) == -1) {
+        if (open((*input_redirect_path), O_RDONLY, 0666) == -1)
+        {
             perror("open");
             exit(1);
         }
     }
-    if ((*output_redirect_path)) {
-        if (close(1) == -1) {
+    if ((*output_redirect_path))
+    {
+        if (close(1) == -1)
+        {
             perror("close");
             exit(1);
         }
         if (open((*output_redirect_path), O_CREAT | O_TRUNC | O_WRONLY, 0666) ==
-            -1) {
+            -1)
+        {
             perror("open");
             exit(1);
         }
-    } else if ((*output_append)) {
-        if (close(1) == -1) {
+    }
+    else if ((*output_append))
+    {
+        if (close(1) == -1)
+        {
             perror("close");
             exit(1);
         }
-        if (open((*output_append), O_CREAT | O_APPEND | O_WRONLY, 0666) == -1) {
+        if (open((*output_append), O_CREAT | O_APPEND | O_WRONLY, 0666) == -1)
+        {
             perror("open");
             exit(1);
         }
     }
 }
 
-int main() {
+int main()
+{
     char buffer[count];
     char *tokens[count / 2];
     char *argv[count / 2];
@@ -252,11 +301,19 @@ int main() {
     char *output_redirect_path = NULL;
     // Create a variable to track input redirect file path
     char *input_redirect_path = NULL;
+    // Create a flag to track wether a process is input for background
+    //int background_flag = 0;
+    // Ignore the following Signals by default
+    signal(SIGINT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
 #ifdef PROMPT
-    if (printf("33sh> ") < 0) {
+    if (printf("33sh> ") < 0)
+    {
         fprintf(stderr, "Error printing prompt to terminal");
     }
-    if (fflush(stdout) < 0) {
+    if (fflush(stdout) < 0)
+    {
         fprintf(stderr, "Error flushing printing terminal prompt");
     }
 #endif
@@ -264,14 +321,17 @@ int main() {
     memset(buffer, '\0', count);
     memset(tokens, '\0', count / 2 * sizeof(char *));
     memset(argv, '\0', count / 2 * sizeof(char *));
-    while ((input_bytes_read = read(0, buffer, count)) != 0) {
+    while ((input_bytes_read = read(0, buffer, count)) != 0)
+    {
         // If no input is read return and reprompt
-        if (input_bytes_read == 0) {
+        if (input_bytes_read == 0)
+        {
             printf("\n");
             return 0;
         }
         // check for read error
-        else if (input_bytes_read == -1) {
+        else if (input_bytes_read == -1)
+        {
             perror("read");
         }
         // Set end of the read input to Null
@@ -280,43 +340,77 @@ int main() {
         parse(buffer, tokens, argv, &argc, &output_append_path,
               &output_redirect_path, &input_redirect_path);
 
-        if (tokens[0] != NULL) {
+        if (tokens[0] != NULL)
+        {
             // Check if the first token matches built ins and handle
             // appropriately
-            if (strcmp(tokens[0], "exit") == 0) {
+            if (strcmp(tokens[0], "exit") == 0)
+            {
                 return 0;
-            } else if (strcmp(tokens[0], "cd") == 0) {
-                if (argc != 2) {
+            }
+            else if (strcmp(tokens[0], "cd") == 0)
+            {
+                if (argc != 2)
+                {
                     fprintf(stderr, "Syntax error with cd");
-                } else if (chdir(argv[1]) != 0) {
+                }
+                else if (chdir(argv[1]) != 0)
+                {
                     perror("chdir");
                 }
-            } else if (strcmp(tokens[0], "ln") == 0) {
-                if (argc != 3) {
+            }
+            else if (strcmp(tokens[0], "ln") == 0)
+            {
+                if (argc != 3)
+                {
                     fprintf(stderr, "Syntax error with ln");
-                } else if (link(argv[1], argv[2]) != 0) {
+                }
+                else if (link(argv[1], argv[2]) != 0)
+                {
                     perror("link");
                 }
-            } else if (strcmp(tokens[0], "rm") == 0) {
-                if (argc != 2) {
+            }
+            else if (strcmp(tokens[0], "rm") == 0)
+            {
+                if (argc != 2)
+                {
                     fprintf(stderr, "Syntax error with rm");
-                } else if (unlink(argv[1]) != 0) {
+                }
+                else if (unlink(argv[1]) != 0)
+                {
                     perror("unlink");
                 }
-            } else {
+            }
+            else
+            {
                 // Execute child process
                 pid_t child_pid = fork();
-                if (child_pid == -1) {
+                if (child_pid == -1)
+                {
                     perror("fork");
                     exit(1);
                 }
-                if (child_pid == 0) {
+                if (child_pid == 0)
+                {
+                    // Restore the following Signals to default
+                    signal(SIGINT, SIG_DFL);
+                    signal(SIGTSTP, SIG_DFL);
+                    signal(SIGTTOU, SIG_DFL);
+                    // Find the unique Process id
+                    pid_t current_pid = getpid();
+                    if (setpgid(current_pid, current_pid) == -1)
+                    {
+                        perror("setpgid");
+                        exit(1);
+                    }
                     io_redirection(&input_redirect_path, &output_redirect_path,
                                    &output_append_path);
                     execv(tokens[0], argv);
                     perror("execv");
                     exit(1);
-                } else {
+                }
+                else
+                {
                     wait(0);
                 }
             }
@@ -330,10 +424,12 @@ int main() {
         input_redirect_path = NULL;
 
 #ifdef PROMPT
-        if (printf("33sh> ") < 0) {
+        if (printf("33sh> ") < 0)
+        {
             fprintf(stderr, "Error printing prompt to terminal");
         }
-        if (fflush(stdout) < 0) {
+        if (fflush(stdout) < 0)
+        {
             fprintf(stderr, "Error flushing printing terminal prompt");
         }
 #endif
